83bcdddb996d3beba3fc44232c42de5c0886ad8e
mvpc-11232 <mvp2612@hotmail.com>
2022-12-13 09:15:03 +0530

    asdf

--- cluster.tf
+++ cluster.tf
@@ -7,8 +7,6 @@ resource "aws_docdb_cluster" "docdb" {
   db_subnet_group_name  = aws_docdb_subnet_group.docdb.name
 }
 
-
-
 # Creates a subnet group , where our cluster will be hosted on
 resource "aws_docdb_subnet_group" "docdb" {
   name       = "roboshop-docdb-${var.ENV}"
@@ -20,9 +18,9 @@ resource "aws_docdb_subnet_group" "docdb" {
 }
 
 # Creates the nodes needed for the created DOCDB Cluster
-resource "aws_docdb_cluster_instance" "cluster_instances" {
-  count              = 1
-  identifier         = "roboshop-docdb-${var.ENV}"
-  cluster_identifier = aws_docdb_cluster.docdb.id
-  instance_class     = "db.t3.medium"
-}
+# resource "aws_docdb_cluster_instance" "cluster_instances" {
+#   count              = 1
+#   identifier         = "roboshop-docdb-${var.ENV}"
+#   cluster_identifier = aws_docdb_cluster.docdb.id
+#   instance_class     = "db.t3.medium"
+# }
