a3882a9d89d70427f8b8903c88346dfdcf348591
mvpc-11232 <mvp2612@hotmail.com>
2022-12-13 09:08:02 +0530

    asdf

--- data.tf
+++ data.tf
@@ -1,6 +1,6 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
-  config {
+  config = {
     bucket = "b51-tf-remote-state-bucket"
     key    = "vpc/${var.ENV}/terraform.tfstate"
     region = "us-east-1"
