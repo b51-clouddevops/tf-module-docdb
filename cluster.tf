8032b15f3f1ec497c33a34d041ed84a37bd55921
mvpc-11232 <mvp2612@hotmail.com>
2022-12-13 09:10:49 +0530

    Auto Push Commit

--- cluster.tf
+++ cluster.tf
@@ -1,11 +1,12 @@
 # # Creates DocDB Cluster
-# resource "aws_docdb_cluster" "docdb" {
-#   cluster_identifier      = "roboshop-${var.ENV}"
-#   engine                  = "docdb"
-#   master_username         = "admin1"
-#   master_password         = "roboshop1"
-#   skip_final_snapshot     = false                     # Make it false in lab only
-# }
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier = "roboshop-${var.ENV}"
+
+  master_username    = "foo"
+  master_password    = "barbut8chars"
+}
+
+
 
 # Creates a subnet group , where our cluster will be hosted on
 resource "aws_docdb_subnet_group" "docdb" {
