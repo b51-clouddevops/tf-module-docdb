b56de558ea3b67520c9d144702f7a518cb432428
mvpc-11232 <mvp2612@hotmail.com>
2022-12-15 08:21:27 +0530

    Auto Push Commit

--- cluster.tf
+++ cluster.tf
@@ -6,6 +6,7 @@ resource "aws_docdb_cluster" "docdb" {
   master_password        = "roboshop1"
   db_subnet_group_name   = aws_docdb_subnet_group.docdb.name
   vpc_security_group_ids = [aws_security_group.allows_docdb.id]
+  skip_final_snapshot     = true
 }
 
 # Creates a subnet group , where our cluster will be hosted on
