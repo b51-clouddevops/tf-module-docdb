ab3071571c61a1131f6d397c08285f2cb8346329
mvpc-11232 <mvp2612@hotmail.com>
2022-12-16 07:55:26 +0530

    Auto Push Commit

--- schema.tf
+++ schema.tf
@@ -4,7 +4,8 @@ resource "null_resource" "schema" {
 cd /tmp 
 curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
 unzip -o mongodb.zip 
-cd mongodn-main 
+cd mongodb-main 
+wget https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
 
 
 EOF 
