9bcb5589e3e56f9cc55294430543eb657a159f91
mvpc-11232 <mvp2612@hotmail.com>
2022-12-16 08:04:22 +0530

    asdf

--- schema.tf
+++ schema.tf
@@ -1,15 +1,16 @@
-resource "null_resource" "mongod-schema" {
+# Loads the schema
+resource "null_resource" "mongodb-schema" { 
+
   provisioner "local-exec" {
-command = <<EOF 
-        cd /tmp/
-        curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
-        unzip -o mongodb.zip 
-        cd mongodb-main 
-        wget https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
-        mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint}:27017 --sslCAFile rds-combined-ca-bundle.pem --username admin1 --password roboshop1  < catalogue.js
-        mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint}:27017 --sslCAFile rds-combined-ca-bundle.pem --username admin1 --password roboshop1  < users.js
-
-EOF 
+command = <<EOF
+  cd /tmp/
+  curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+  wget https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
+  unzip -o mongodb.zip 
+  cd mongodb-main 
+  mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint}:27017 --sslCAFile /tmp/rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < catalogue.js
+  mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint}:27017 --sslCAFile /tmp/rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < users.js
+  
+EOF   
   }
-
 }
