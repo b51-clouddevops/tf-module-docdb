a471123b972dc26768cc6df81c4021642f8721c8
mvpc-11232 <mvp2612@hotmail.com>
2022-12-16 07:43:29 +0530

    Auto Push Commit

--- schema.tf
+++ schema.tf
@@ -1,8 +1,8 @@
 resource "null_resource" "schema" {
   provisioner "local-exec" {
     command = <<EOF 
-
-    
+cd /tmp 
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo    
 
 EOF 
   }
