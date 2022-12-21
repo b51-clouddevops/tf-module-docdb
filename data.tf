2d62e13790744b82ce66ecb18de28248c1d9745d
mvpc-11232 <mvp2612@hotmail.com>
2022-12-21 08:00:57 +0530

    asdf

--- data.tf
+++ data.tf
@@ -6,3 +6,11 @@ data "terraform_remote_state" "vpc" {
     region = "us-east-1"
   }
 }
+
+data "aws_secretsmanager_secret_version" "secrets" {
+  secret_id = "robot/secrets"
+}
+
+output "data-op" {
+  value = data.aws_secretsmanager_secret_version.secrets
+}
